a6b32d3981d037208cb9a74133cab68b4fb018d6
Kelamir <kelamir@protonmail.com>
2022-12-28 18:11:27 +0200

    Add a button and function that handles the start of a pomodoro session.

--- src/routes/pomodoro/+page.svelte
+++ src/routes/pomodoro/+page.svelte
@@ -6,16 +6,23 @@
 
 <script>
     let pomLength = 25;
+
+    function startPom() {
+        console.log("imagine it started.")
+    }
 </script>
 
-<div>
-    <h1>Pomodoro productivity timer.</h1>
 
-    <label>
-        <input type="number" bind:value={pomLength} min=10, max=120>
-        <input type="range" bind:value={pomLength} min=10, max=120>
-    </label>
-</div>
+<h1>Pomodoro productivity timer.</h1>
+
+<label>
+    <input type="number" bind:value={pomLength} min=10, max=120>
+    <input type="range" bind:value={pomLength} min=10, max=120>
+</label>
+
+<button on:click={startPom}>
+    start pomodoro
+</button>
 
 
 
