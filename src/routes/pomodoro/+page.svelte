f89554bf40ef168f5edd170e35980ae7fb5a54c4
Kelamir <kelamir@protonmail.com>
2022-12-28 17:46:06 +0200

    Remove step parameter which causes an issue with min max values.

--- src/routes/pomodoro/+page.svelte
+++ src/routes/pomodoro/+page.svelte
@@ -12,8 +12,8 @@
     <h1>Pomodoro productivity timer.</h1>
 
     <label>
-        <input type="number" bind:value={pomLength} min=10, max=120, step=5>
-        <input type="range" bind:value={pomLength} min=10, max=120, step=5>
+        <input type="number" bind:value={pomLength} min=10, max=120>
+        <input type="range" bind:value={pomLength} min=10, max=120>
     </label>
 </div>
 
