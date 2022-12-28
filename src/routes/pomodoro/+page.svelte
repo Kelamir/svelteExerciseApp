e967ada89509bedc8c0d731038101bef576f2832
Kelamir <kelamir@protonmail.com>
2022-12-28 17:43:28 +0200

    Add an option to pick pomodoro length.

--- src/routes/pomodoro/+page.svelte
+++ src/routes/pomodoro/+page.svelte
@@ -1,7 +1,24 @@
 <svelte:head>
     <title>Experiment</title>
-    <meta name="description" content="Pomodoro productivity timer"/>
+    <meta name="description" content="Pomodoro"/>
 </svelte:head>
-<script>
 
+
+<script>
+    let pomLength = 25;
 </script>
+
+<div>
+    <h1>Pomodoro productivity timer.</h1>
+
+    <label>
+        <input type="number" bind:value={pomLength} min=10, max=120, step=5>
+        <input type="range" bind:value={pomLength} min=10, max=120, step=5>
+    </label>
+</div>
+
+
+
+<style>
+
+</style>
