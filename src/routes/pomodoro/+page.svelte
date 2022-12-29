aef3c1425aec65c12cf96e7c74bd587b89c2fff8
Kelamir <kelamir@protonmail.com>
2022-12-29 14:58:29 +0200

    Add steps to limit range of available numbers for pom length

--- src/routes/pomodoro/+page.svelte
+++ src/routes/pomodoro/+page.svelte
@@ -18,7 +18,7 @@
     <h1>Pomodoro productivity timer.</h1>
 
     <label>
-        <input type="range" bind:value={inputMinutes} min=10 max=120>
+        <input type="range" bind:value={inputMinutes} min=10 max=120 step=5>
     </label>
     <p>Pom length: {inputMinutes}</p>
     <p>difference: {(end - start) / 60} minutes</p>
