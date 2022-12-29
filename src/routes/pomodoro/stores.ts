b1191b9fa6b65525120b34b4863de9b36a0d1ed7
Kelamir <kelamir@protonmail.com>
2022-12-30 00:11:25 +0200

    Use derived store for countdown

--- src/routes/pomodoro/stores.ts
+++ src/routes/pomodoro/stores.ts
@@ -1,6 +1,6 @@
-import { writable } from 'svelte/store'
+import {derived, writable} from 'svelte/store'
 
 export let interval;
 export const inputMinutes = writable(25);
-export const countdown = writable((inputMinutes) * 60);
+export const countdown = derived(inputMinutes, $inputMinutes => ($inputMinutes * 60));
 export const started = writable(false);
