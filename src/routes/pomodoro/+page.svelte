c40bb79eb91dbf8359a02f55ea4c82b8c8c73ca1
Kelamir <kelamir@protonmail.com>
2022-12-29 18:32:11 +0200

    Change visible menu upon pom start.

--- src/routes/pomodoro/+page.svelte
+++ src/routes/pomodoro/+page.svelte
@@ -7,11 +7,14 @@
 <script>
     import {writable} from "svelte/store";
 
+    let started = false;
+
     let interval;
     let inputMinutes = 25;
     $: countdown = writable(inputMinutes * 60);
 
     function startPom() {
+        started = !started;
         let start = new Date().getTime() / 1000;
         let end = start + (inputMinutes * 60);
 
@@ -28,15 +31,21 @@
 <div class="text-column">
     <h1>Pomodoro productivity timer.</h1>
 
-    <label>
-        <input type="range" bind:value={inputMinutes} min=10 max=120 step=5>
-    </label>
-    <p>Pom length: {inputMinutes}</p>
+    {#if (!started)}
+        <label>
+            <input type="range" bind:value={inputMinutes} min=9 max=120 step=5>
+        </label>
+        <p>Pom length: {inputMinutes}</p>
 
-    <button on:click={startPom}>
-        Start
-    </button>
-    <p>Countdown: {$countdown}</p>
+        <button on:click={startPom}>
+            Start
+        </button>
+        <p>Countdown: {$countdown}</p>
+
+        {:else}
+        <p>Pom started.</p>
+    {/if}
+
 </div>
 
 
