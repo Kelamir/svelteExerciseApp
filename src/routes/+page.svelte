827e253d5ac05a32074c9b099701bedbcc38ec23
Kelamir <kelamir@protonmail.com>
2023-01-05 15:39:01 +0200

    Update the project overall to be focused on pomodoro app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,40 @@
+
+
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    import {isFinished, isStarted} from "../stores.js";
+
+    import SetDuration from "../components/SetDuration.svelte";
+    import InProgressPomodoro from "../components/InProgressPomodoro.svelte";
+    import FinishedMenu from "../components/FinishedMenu.svelte";
+
+    import StartStopButton from "../components/StartStopButton.svelte";
 </script>
 
+
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+    <title>Experiment</title>
+    <meta name="description" content="Pomodoro"/>
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<div class="text-column">
+    <h1>Pomodoro productivity timer.</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+    {#if $isStarted}
+        <InProgressPomodoro />
+    {:else if $isFinished}
+        <FinishedMenu />
+        <SetDuration />
+    {:else}
+        <SetDuration />
+    {/if}
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+    <StartStopButton />
 
-	<Counter />
-</section>
+
+</div>
+
+
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    div { display: flex; flex-direction: column}
 </style>
